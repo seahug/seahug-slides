03d4bd04b15a67346945f501d5e774ce75bdbe85
Richard Cook <rcook@rcook.org>
2015-03-09 21:51:38 -0700

    Fix up project directory in scripts

--- script/run.cmd
+++ script/run.cmd
@@ -2,10 +2,15 @@
 setlocal
 call :SetConfig %~dp0\.
 
-pushd %SCRIPTDIR%
+pushd %PROJECTDIR%
 cabal run
 goto :eof
 
 :SetConfig
 set SCRIPTDIR=%~df1
+call :SetConfig0 %SCRIPTDIR%\..
 goto :eof
+:SetConfig0
+set PROJECTDIR=%~df1
+goto :eof
+
